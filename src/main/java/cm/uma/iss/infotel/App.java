3eba417d859a1a6737ca49a3dd95c69d1cef047f
Vincent Douwe <douwevincent@yahoo.fr>
2017-03-31 11:34:10 +0100

    Revert "J'ai changé le message en francais"
    This reverts commit 437cd1252bb3f56f1f7cde6fbf89b4bdda486b87.

--- src/main/java/cm/uma/iss/infotel/App.java
+++ src/main/java/cm/uma/iss/infotel/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour à tout le monde" );
+        System.out.println( "Hello World!" );
     }
 }
